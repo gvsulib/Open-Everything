da35ee09526a6f79967bdef311cd6e8eec270ac1
Matt Reidsma <mreidsma@Matthews-MacBook-3.local>
2014-09-11 13:54:26 -0400

    Placebo effect for folks who submit complete form is JS fails

--- record.php
+++ record.php
@@ -2,11 +2,21 @@
 /* Script to record interaction choices */
 header("Access-Control-Allow-Origin: *");
 
-$record = array($_POST['time'], $_POST['answer']);
+if(isset($_POST['no-js'])) {
+	
+	// Not really recoridng anything, just sending them back to the main page
+	header('Location: http://gvsu.edu/library/openeverything/');
+	exit;
+	
+} else {
 
-// More sane CSV code thanks to Michael Oehrlich, oehrlich@thulb.uni-jena.de
+	$record = array($_POST['time'], $_POST['answer']);
 
-if (!$DataFile = fopen("data.csv", "a")) {echo "Failure: cannot open file"; die;};
-if (!fputcsv($DataFile, $record)) {echo "Failure: cannot write to file"; die;};
-fclose($DataFile);
-echo "file write successful";
+	// More sane CSV code thanks to Michael Oehrlich, oehrlich@thulb.uni-jena.de
+
+	if (!$DataFile = fopen("data.csv", "a")) {echo "Failure: cannot open file"; die;};
+	if (!fputcsv($DataFile, $record)) {echo "Failure: cannot write to file"; die;};
+	fclose($DataFile);
+	echo "file write successful";
+	
+}
